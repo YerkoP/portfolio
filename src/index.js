5c716b77215f6572291f34b7e00f3c44f99aa4fd
Yerko Palma <yerko.palma@usach.cl>
2024-11-08 23:41:11 -0300

    feat: glow button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import './style.css'
 import './cursor'
 import './md-content'
+import chroma from "chroma-js"
+import gsap from "gsap"
+
+const generateGlowButtons = () => {
+  document.querySelectorAll(".glow-button").forEach((button) => {
+      let gradientElem = button.querySelector('.gradient');
+      
+      if(!gradientElem) {
+          gradientElem = document.createElement("div");
+          gradientElem.classList.add("gradient");
+
+          button.appendChild(gradientElem);
+      }
+
+      button.addEventListener("pointermove", (e) => {
+          const rect = button.getBoundingClientRect();
+
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+
+          gsap.to(button, {
+              "--pointer-x": `${x}px`,
+              "--pointer-y": `${y}px`,
+              duration: 0.6,
+          });
+
+          gsap.to(button, {
+              "--button-glow": chroma
+              .mix(
+                  getComputedStyle(button)
+                  .getPropertyValue("--button-glow-start")
+                  .trim(),
+                  getComputedStyle(button).getPropertyValue("--button-glow-end").trim(),
+                  x / rect.width
+              )
+              .hex(),
+              duration: 0.2,
+          });
+      });
+  });
+}
 
 ;(function() {
   const expansionItems = Array.from(document.querySelectorAll('[data-expand]'))
@@ -35,5 +76,7 @@ import './md-content'
     console.log('[doc] leaving link')
     cursor.removeClass('expanded')
   })
+
+  generateGlowButtons()
 })()
 
